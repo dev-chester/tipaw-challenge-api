ebba47df9a045a09a1c4e77c9d265b1ccaf10e00
Chester Arellano <54052060+dev-chester@users.noreply.github.com>
2022-07-04 16:23:37 +0800

    fixed regex in password pattern matching

--- controllers/account.validator.ts
+++ controllers/account.validator.ts
@@ -7,8 +7,30 @@ export const register = {
             last_name: Joi.string().required(),
             phone_number: Joi.string().required(),
             email:Joi.string().email().required(),
-            password: Joi.string().min(8).pattern(new RegExp('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$')),
+            password: Joi.string().min(8).pattern(new RegExp('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=?:]).*$')),
             has_accepted_terms: Joi.boolean().required(),
         }).required()
     })
+}
+
+
+{
+    "data": {
+        "first_name": "asdf",
+        "last_name": "Arellano",
+        "phone_number": "324242234234243",
+        "email": "chester@gmail.co3m",
+        "password": "asdF3^asdff",
+        "has_accepted_terms": true
+    }
+}
+
+{"data" :{
+    "first_name":"Chester",
+    "last_name":"Arellano",
+    "phone_number":"667767565",
+    "email":"test@gmail.com",
+    "password":"asdfasdfQ1!",
+    "has_accepted_terms":true
+}
 }
