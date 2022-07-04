dcf56d8125dbd05dd6920ccfd00d2bc6538c5dfc
Chester Arellano <54052060+dev-chester@users.noreply.github.com>
2022-07-04 16:24:57 +0800

    fixed regex on password pattern

--- controllers/account.validator.ts
+++ controllers/account.validator.ts
@@ -12,25 +12,3 @@ export const register = {
         }).required()
     })
 }
-
-
-{
-    "data": {
-        "first_name": "asdf",
-        "last_name": "Arellano",
-        "phone_number": "324242234234243",
-        "email": "chester@gmail.co3m",
-        "password": "asdF3^asdff",
-        "has_accepted_terms": true
-    }
-}
-
-{"data" :{
-    "first_name":"Chester",
-    "last_name":"Arellano",
-    "phone_number":"667767565",
-    "email":"test@gmail.com",
-    "password":"asdfasdfQ1!",
-    "has_accepted_terms":true
-}
-}
